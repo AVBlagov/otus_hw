9a3f4eb2dd2732a40c8a9f2982ae01899ad88910
Alex <avblagov@gmail.com>
2020-09-01 23:06:38 +0400

    hw01 1

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -1,10 +1,11 @@
-module github.com/fixme_my_friend/hw01_hello_now
+module github.com/AVBlagov/otus_hw/hw01_hello_now
 
-go 1.14
+go 1.13
 
 require (
 	bou.ke/monkey v1.0.2
-	github.com/beevik/ntp v0.2.0
+	github.com/beevik/ntp v0.3.0
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200831180312-196b9ba8737a // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+//List of ntp servers
+const (
+	rupool     = "ru.pool.ntp.org"
+	beevikpool = "0.beevik-ntp.pool.ntp.org"
+	vniiftri   = "ntp1.vniiftri.ru"
+)
+
 func main() {
-	// Place your code here
+
+	fmt.Println("current time:", time.Now().Round(0))
+
+	if t, err := ntp.Time(vniiftri); err != nil {
+		log.Fatalf("Error of ntp server: %v", err)
+	} else {
+		fmt.Println("exact time:", t.Round(0))
+	}
+
 }

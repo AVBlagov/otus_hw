30cc9920094ce0f9c173b2f3631c917027d6550a
Alex <avblagov@gmail.com>
2020-09-01 23:47:07 +0400

    hw01 4

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -8,18 +8,11 @@ import (
 	"github.com/beevik/ntp"
 )
 
-//List of ntp servers
-const (
-	//rupool     = "ru.pool.ntp.org"
-	//beevikpool = "0.beevik-ntp.pool.ntp.org"
-	vniiftri = "ntp1.vniiftri.ru"
-)
-
 func main() {
 
 	fmt.Println("current time:", time.Now().Round(0))
 
-	t, err := ntp.Time(vniiftri)
+	t, err := ntp.Time("ntp1.vniiftri.ru")
 	if err != nil {
 		log.Fatalf("Error of ntp server: %v", err)
 	} else {
